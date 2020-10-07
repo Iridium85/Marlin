4c17573121e94e2c658ed292e5cde02682ba78fc
Iridium85 <66847775+Iridium85@users.noreply.github.com>
2020-10-07 14:47:09 +0200

    New steps/mm
    New steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 53.41, 53.43, 1742, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 53.32, 53.32, 1742, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
